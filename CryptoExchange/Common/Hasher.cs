9e139e9f1ee08795be2200b1c91fc2c8aee907db
VengerAndrey <vengerandr@gmail.com>
2021-09-20 12:30:20 +0300

    Add sign up.

---
+++ CryptoExchange/Common/Hasher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoExchange.Common
+{
+    public static class Hasher
+    {
+        public static string Hash(string data)
+        {
+            using var sha256 = SHA256.Create();
+            var bytes = sha256.ComputeHash(Encoding.Unicode.GetBytes(data));
+            var stringBuilder = new StringBuilder();
+            foreach (var b in bytes)
+            {
+                stringBuilder.Append(b.ToString("X2"));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- CryptoExchange/Controllers/AuthController.cs
+++ CryptoExchange/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CryptoExchange.Common;
 using CryptoExchange.Data;
 using CryptoExchange.Models;
 using Microsoft.EntityFrameworkCore;
@@ -29,11 +30,42 @@ namespace CryptoExchange.Controllers
         [HttpPost]
         public async Task<IActionResult> SignIn(User user)
         {
-            // implement hashing
             var existingUser = await _context.Users.FirstOrDefaultAsync(x => x.Email == user.Email &&
-                                                                             x.Password == user.Password);
+                                                                             x.Password == Hasher.Hash(user.Password));
             if (existingUser != null)
             {
+                return new RedirectResult("/");
+            }
+
+            return new RedirectResult("/Error");
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(User user)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(x => x.Email == user.Email);
+
+            if (existingUser == null)
+            {
+                user.SignUpTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                user.Password = Hasher.Hash(user.Password);
+
+                try
+                {
+                    await _context.Users.AddAsync(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    return new RedirectResult("/Error");
+                }
+
                 return new RedirectResult("/");
             }
 
